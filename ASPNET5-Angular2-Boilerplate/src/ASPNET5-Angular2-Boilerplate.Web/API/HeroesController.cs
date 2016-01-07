760cf2cace65843c9a20090100096cfd5343f36d
larrynburris <larry.n.burris@gmail.com>
2016-01-07 17:11:55 -0500

    Add ASPNET5-Angular2-Boilerplate solution

---
+++ ASPNET5-Angular2-Boilerplate/src/ASPNET5-Angular2-Boilerplate.Web/API/HeroesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ASPNET5_Angular2_Boilerplate.Web.Models;
+using ASPNET5_Angular2_Boilerplate.Web.Classes;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNET5_Angular2_Boilerplate.Web.API
+{
+    [Route("api/[controller]")]
+    public class HeroesController : Controller
+    {
+        // GET: api/heroes
+        [HttpGet]
+        public IEnumerable<DOTAHero> Get()
+        {
+            var hm = new HeroManager();
+            return hm.GetAll;
+        }
+
+        // GET api/heroes/{id}
+        [HttpGet("{id}")]
+        public DOTAHero Get(int id)
+        {
+            var hm = new HeroManager();
+            return hm.GetHeroByID(id);
+        }
+    }
+}
---
+++ ASPNET5-Angular2-Boilerplate/src/ASPNET5-Angular2-Boilerplate.Web/Scripts/app.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: '<p>My First Angular 2 App</p>',
+})
+export class AppComponent { }
