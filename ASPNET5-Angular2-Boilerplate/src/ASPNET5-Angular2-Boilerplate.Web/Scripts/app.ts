400e5b0084fe34b7579437dbe94fb279c4adcc2f
larrynburris <larry.n.burris@gmail.com>
2016-01-07 17:36:41 -0500

    Angular 2 Tutorial Pt 1 Complete

--- ASPNET5-Angular2-Boilerplate/src/ASPNET5-Angular2-Boilerplate.Web/Scripts/app.ts
+++ ASPNET5-Angular2-Boilerplate/src/ASPNET5-Angular2-Boilerplate.Web/Scripts/app.ts
@@ -1,6 +1,25 @@
 ﻿import { Component } from 'angular2/core';
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<p>My First Angular 2 App</p>',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        </div>
+    `,
 })
-export class AppComponent { }
+export class AppComponent{
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
