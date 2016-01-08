0dd7a27dc3218ffd305447454ee6b44aecbadd59
larrynburris <larry.n.burris@gmail.com>
2016-01-07 21:45:06 -0500

    Angular 2 Tutorial Pt 4 Complete

---
+++ ASPNET5-Angular2-Boilerplate/src/ASPNET5-Angular2-Boilerplate.Web/Scripts/hero.service.ts
@@ -0,0 +1,17 @@
+﻿import {Hero} from './hero'
+import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
+
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000)
+        );
+    }
+}
